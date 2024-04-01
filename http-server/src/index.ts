844353d3d55c06e57309dbce5b227304906a4842
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-04-01 17:14:41 +0800

    Add readiness and liveness probes

--- http-server/src/index.ts
+++ http-server/src/index.ts
@@ -1,12 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import logger from './logger';
 
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.get("/", async function (req, res) {
   logger.info("GET request")
   res.send("Hello world");
+});
+
+app.get('/healthz', (req: Request, res: Response): void => {
+  logger.info("Health probe")
+  res.send('OK');
+});
+
+app.get('/ready', (req: Request, res: Response): void => {
+  logger.info("Readiness probe")
+  res.send('OK');
 });
 
 app.listen(PORT, async function () {
