87b367bff001e17dc81f75f7791806009fffa12d
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-04-03 10:39:46 +0800

    Log format change

--- http-server/src/logger.ts
+++ http-server/src/logger.ts
@@ -1,19 +1,11 @@
-import winston, { format, createLogger, transports } from 'winston';
-
-const customFormat = winston.format.printf((info) => {
-  return JSON.stringify({
-    time: (new Date()).toISOString(),
-    level: info.level,
-    message: info.message,
-  });
-});
+import { format, createLogger, transports } from 'winston';
 
 // Configure the logger
 const logger = createLogger({
   level: 'info',
   format: format.combine(
     format.timestamp(),
-    customFormat,
+    format.json(),
   ),
   transports: [
     new transports.Console()
